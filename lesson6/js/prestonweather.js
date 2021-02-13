43680bdf92e627c4656de981b87ddeff9fb031df
rachelblock2 <lof16002@byui.edu>
2021-02-12 21:18:40 -0700

    Changed hero image for web optimization

---
+++ lesson6/js/prestonweather.js
@@ -0,0 +1,26 @@
+// Creates banner for free pancake reminder on Friday
+let date = new Date();
+let weekday = date.toLocaleDateString("default", { weekday: 'long' });
+
+let message1 = "";
+
+if (weekday == "Friday") {
+    message1 = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park.";
+    document.querySelector("#pancakes").textContent = message1
+} else {
+  let pancakes = document.querySelector("#pancakes");
+  pancakes.remove()
+}
+
+// Creates a hamburger button for small css file
+const hambutton = document.querySelector('.hamburger_menu');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+//Creates date object updated daily
+const yearoptions = {year: "numeric", month: "long", day: "2-digit"}
+document.getElementById("currentyear").textContent = new Date().toLocaleDateString("en-US", yearoptions);
