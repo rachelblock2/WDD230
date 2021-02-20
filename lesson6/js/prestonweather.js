2de02e92f4e0e2b5c613fcc99d3f1b878a8d3ada
rachelblock2 <lof16002@byui.edu>
2021-02-20 16:49:30 -0700

    Fixed lazyload js

--- lesson6/js/prestonweather.js
+++ lesson6/js/prestonweather.js
@@ -28,39 +28,44 @@ document.getElementById("currentyear").textContent = new Date().toLocaleDateStri
 
 
 
-// //Lazy Loading of gallery images
-// let imagesToLoad = document.querySelectorAll('img[data-src]');
+//Lazy Loading of gallery images
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
-// // Remove placeholder image upon loading the image
-// const loadImages = (image) => {
-//   image.setAttribute('src', image.getAttribute('data-src'));
-//   image.onload = () => {
-//     image.removeAttribute('data-src');
-//   };
-// };
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px, 0px, 50px, 0px"
+};
 
-// // Loop through each image and lazy load it
-// imagesToLoad.forEach((img) => {
-//     loadImages(img);
-//   });
+// Remove placeholder image upon loading the image
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
 
-// // Allows for items to loaded only when visible in the viewport
-//   if('IntersectionObserver' in window) {
-//     const observer = new IntersectionObserver((items, observer) => {
-//       // Loop when there is multiple items, aka a src and an data-src
-//       items.forEach((item) => {
-//         if(item.isIntersecting) {
-//           // Load images when the items interact with observer aka come into the viewport
-//           loadImages(item.target);
-//           observer.unobserve(item.target);
-//         }
-//       });
-//     });
-//     imagesToLoad.forEach((img) => {
-//       observer.observe(img);
-//     });
-//   } else {
-//     imagesToLoad.forEach((img) => {
-//       loadImages(img);
-//     });
-//   }
+// Loop through each image and lazy load it
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+
+// Allows for items to loaded only when visible in the viewport
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      // Loop when there is multiple items, aka a src and an data-src
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          // Load images when the items interact with observer aka come into the viewport
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
