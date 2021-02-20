921cb4e32668ecb977ec314c78a1655e7afabc90
rachelblock2 <lof16002@byui.edu>
2021-02-20 02:11:52 -0700

    Week 7 assignment, added links to previous Preston

--- lesson6/js/prestonweather.js
+++ lesson6/js/prestonweather.js
@@ -24,3 +24,43 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
 //Creates date object updated daily
 const yearoptions = {year: "numeric", month: "long", day: "2-digit"}
 document.getElementById("currentyear").textContent = new Date().toLocaleDateString("en-US", yearoptions);
+
+
+
+
+// //Lazy Loading of gallery images
+// let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// // Remove placeholder image upon loading the image
+// const loadImages = (image) => {
+//   image.setAttribute('src', image.getAttribute('data-src'));
+//   image.onload = () => {
+//     image.removeAttribute('data-src');
+//   };
+// };
+
+// // Loop through each image and lazy load it
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
+
+// // Allows for items to loaded only when visible in the viewport
+//   if('IntersectionObserver' in window) {
+//     const observer = new IntersectionObserver((items, observer) => {
+//       // Loop when there is multiple items, aka a src and an data-src
+//       items.forEach((item) => {
+//         if(item.isIntersecting) {
+//           // Load images when the items interact with observer aka come into the viewport
+//           loadImages(item.target);
+//           observer.unobserve(item.target);
+//         }
+//       });
+//     });
+//     imagesToLoad.forEach((img) => {
+//       observer.observe(img);
+//     });
+//   } else {
+//     imagesToLoad.forEach((img) => {
+//       loadImages(img);
+//     });
+//   }
