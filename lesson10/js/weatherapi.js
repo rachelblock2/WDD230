0bdf16fbd9f40a11c280ce46ae3b257db27c219e
rachelblock2 <lof16002@byui.edu>
2021-03-13 08:50:48 -0700

    Added link to assignment 10 on my portal page

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -8,7 +8,7 @@ fetch(apiURL)
     temperature.textContent = jsObject.main.temp;
 
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    const desc = jsObject.weather[0].description;  // reference the weather array
     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
     document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
     document.getElementById('icon').setAttribute('alt', desc);
