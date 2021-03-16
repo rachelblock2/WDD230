9c5b1cc7c69926dfc52abbd2a863b428ae8f2ed0
rachelblock2 <lof16002@byui.edu>
2021-03-16 13:27:37 -0600

    Updated links and titles, working on js

--- lesson11/js/prestonweather.js
+++ lesson11/js/prestonweather.js
@@ -161,10 +161,8 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
 
-    // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    // const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.querySelector('#current_1').textContent = jsObject.main.temp;
-    document.querySelector('#current_2').textContent = jsObject.main.temp_max;
+    document.querySelector('#current_1').textContent = jsObject.weather[0].description;
+    document.querySelector('#current_2').textContent = (Math.round(jsObject.main.temp));
     document.querySelector('#current_4').textContent = jsObject.main.humidity;
     document.querySelector('#current_5').textContent = jsObject.wind.speed;
   });
@@ -188,3 +186,77 @@ fetch(apiForecast)
       document.querySelector(`#forecast${step+1}`).textContent = (Math.round(forecast[step].main.temp));
     };
   });
+
+  
+
+// ------------- Consume Weather API for Fish Haven Page ---------------- //
+
+const apiURLFH = "https://api.openweathermap.org/data/2.5/weather?id=5585010&APPID=b850fbcd027801228eb544e5bbb816db&units=imperial"
+
+fetch(apiURLFH)
+  .then((response) => response.json())
+  .then((jsObject) => {
+
+    document.querySelector('#fh_current_1').textContent = jsObject.weather[0].description;
+    document.querySelector('#fh_current_2').textContent = (Math.round(jsObject.main.temp));
+    document.querySelector('#fh_current_4').textContent = jsObject.main.humidity;
+    document.querySelector('#fh_current_5').textContent = jsObject.wind.speed;
+  });
+
+const apiForecastFH = "https://api.openweathermap.org/data/2.5/forecast?id=5585010&APPID=b850fbcd027801228eb544e5bbb816db&units=imperial"
+
+fetch(apiForecastFH)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00')); // Gets a new array from all forecast days at 1800 hours
+
+    const dayofWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    for (let step = 0; step < 5; step++) {
+      // Loop through each of the next 5 forecast days
+      let d = new Date(forecast[step].dt_txt); // Creates a new date with the information from the JSON
+      let image = 'https://openweathermap.org/img/w/' + forecast[step].weather[0].icon + '.png';
+      document.querySelector(`#fh_dayoftheweek${step+1}`).textContent = dayofWeek[d.getDay()];
+      document.querySelector(`#fh_img${step+1}`).setAttribute('src', image)
+      document.querySelector(`#fh_img${step+1}`).setAttribute('alt', forecast[step].weather[0].description)
+      document.querySelector(`#fh_forecast${step+1}`).textContent = (Math.round(forecast[step].main.temp));
+    };
+  });
+
+
+
+// ------------- Consume Weather API for Soda Springs Page ---------------- //
+
+const apiURLSS = "https://api.openweathermap.org/data/2.5/weather?id=5607916&APPID=b850fbcd027801228eb544e5bbb816db&units=imperial"
+
+fetch(apiURLSS)
+  .then((response) => response.json())
+  .then((jsObject) => {
+
+    document.querySelector('#ss_current_1').textContent = jsObject.weather[0].description;
+    document.querySelector('#ss_current_2').textContent = (Math.round(jsObject.main.temp));
+    document.querySelector('#ss_current_4').textContent = jsObject.main.humidity;
+    document.querySelector('#ss_current_5').textContent = jsObject.wind.speed;
+  });
+
+const apiForecastSS = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&APPID=b850fbcd027801228eb544e5bbb816db&units=imperial"
+
+fetch(apiForecastSS)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00')); // Gets a new array from all forecast days at 1800 hours
+
+    const dayofWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    for (let step = 0; step < 5; step++) {
+      // Loop through each of the next 5 forecast days
+      let d = new Date(forecast[step].dt_txt); // Creates a new date with the information from the JSON
+      let image = 'https://openweathermap.org/img/w/' + forecast[step].weather[0].icon + '.png';
+      document.querySelector(`#ss_dayoftheweek${step+1}`).textContent = dayofWeek[d.getDay()];
+      document.querySelector(`#ss_img${step+1}`).setAttribute('src', image)
+      document.querySelector(`#ss_img${step+1}`).setAttribute('alt', forecast[step].weather[0].description)
+      document.querySelector(`#ss_forecast${step+1}`).textContent = (Math.round(forecast[step].main.temp));
+    };
+  });
+
+// 5607916 for Soda Springs
--- lesson11/js/windchill.js
+++ lesson11/js/windchill.js
@@ -1,5 +1,5 @@
-// ----------- Calculate current windchill --------- //
-temperature = document.querySelector("#current_1").innerHTML
+// ----------- Calculate current windchill on Preston Page--------- //
+temperature = document.querySelector("#current_2").innerHTML
 windSpeed = document.querySelector("#current_5").innerHTML
 
 if (temperature < 50 && windSpeed > 3) {
@@ -9,6 +9,24 @@ if (temperature < 50 && windSpeed > 3) {
     document.querySelector("#total_wind_chill").innerHTML = "N/A"
 }
 
+
+
+// ----------- Calculate current windchill on Fish Haven Page--------- //
+fhTemperature = document.querySelector("#fh_current_2").innerHTML
+fhWindSpeed = document.querySelector("#fh_current_5").innerHTML
+
+console.log(fhTemperature)
+console.log(fhWindSpeed)
+
+if (fhTemperature < 50 && fhWindSpeed > 3) {
+    totalWindChill = calcWindchill(fhTemperature, fhWindSpeed)
+    document.querySelector("#fh_total_wind_chill").innerHTML = totalWindChill + "\u00B0"
+} else {
+    document.querySelector("#fh_total_wind_chill").innerHTML = "N/A"
+}
+
+
+// ------- Windchill calculation function ----------//
 function calcWindchill(currentTemp, currentWindSpeed) {
     windChill = 35.74 + (0.6215 * currentTemp) - (35.74 * (currentWindSpeed ** 0.16)) + (0.4275 * currentTemp * (currentWindSpeed ** 0.16))
     roundedWindChill = windChill.toFixed(0)
