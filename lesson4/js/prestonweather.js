a06e0e0872e77d9d6b6ba631253f774cc8bbdef5
rachelblock2 <lof16002@byui.edu>
2021-10-15 15:20:18 -0600

    Removed an unused function

--- lesson4/js/prestonweather.js
+++ lesson4/js/prestonweather.js
@@ -5,7 +5,9 @@
 const hambutton = document.querySelector('.hamburger_menu');
 const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive')
+}, false);
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
